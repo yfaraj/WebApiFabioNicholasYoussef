8495daa53ae5a6f6135a198e463d1a6c7db904c9
fabiodevx <61858653+fabiodevx@users.noreply.github.com>
2022-03-13 01:19:51 -0600

    Added code to get System Type

--- WebAPI_3/Services/DataBuilderService.cs
+++ WebAPI_3/Services/DataBuilderService.cs
@@ -19,13 +19,41 @@ namespace WebAPI_3.Services
         private const string Manufacturer_Recall_No_Txt = "MANUFACTURER_RECALL_NO_TXT";
         private const string Category_Etxt = "CATEGORY_ETXT";
         private const string Category_Ftxt = "CATEGORY_FTXT";
+        private const string System_Type_Etxt = "SYSTEM_TYPE_ETXT";
+        private const string System_Type_Ftxt = "SYSTEM_TYPE_FTXT";
 
         public static void Init(IHttpClientFactory clientFactory)
         {
             _clientFactory = clientFactory;
         }
 
-        public static async Task<string> LoadDataFromPreviousAPIs()
+        public static async Task<string> GetSystemTypeData(string inputJson)
+        {
+            var inputData = JsonConvert.DeserializeObject<TC_Data_API_3[]>(inputJson).ToList();
+            var outputData = new List<TC_Data_API_3>();
+
+            foreach (var item in inputData)
+            {
+                var vrdData = await GetVrdData(item.recallNumber);
+                
+                if (vrdData == null || vrdData.ResultSet.Count == 0)
+                {
+                    continue;
+                }
+
+                var vrdDataList = vrdData.ResultSet.First();
+                var sytemTypeEtxt = vrdDataList.FirstOrDefault(x => x.Name.Equals(System_Type_Etxt)).Value.Literal;
+                var sytemTypeFtxt = vrdDataList.FirstOrDefault(x => x.Name.Equals(System_Type_Ftxt)).Value.Literal;
+                item.systemTypeETXT = sytemTypeEtxt;
+                item.systemTypeFTXT = sytemTypeFtxt;
+
+                outputData.Add(item);
+            }
+
+            return JsonConvert.SerializeObject(outputData);
+        }
+
+            public static async Task<string> LoadDataFromPreviousAPIs()
         {
             try
             {
@@ -34,7 +62,7 @@ namespace WebAPI_3.Services
                 var initialJsonFile = File.ReadAllText(fullPath);
 
                 var tc_Data = JsonConvert.DeserializeObject<TC_Data[]>(initialJsonFile);
-                List<TC_Data_API_2> tcApiDataList = new List<TC_Data_API_2>();
+                var tcApiDataList = new List<TC_Data_API_2>();
 
                 foreach (var item in tc_Data)
                 {
@@ -50,7 +78,7 @@ namespace WebAPI_3.Services
                     var categoryFTXT = vrdDataList.FirstOrDefault(x => x.Name.Equals(Category_Ftxt)).Value.Literal;
 
 
-                    tcApiDataList.Add(new TC_Data_API_3()
+                    tcApiDataList.Add(new TC_Data_API_2()
                     {
                         recallNumber = item.recallNumber,
                         manufactureName = item.manufactureName,
