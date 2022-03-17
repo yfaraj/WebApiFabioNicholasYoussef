3bd1090182be1451231069b14d6b319fc4e2a104
fabiodevx <61858653+fabiodevx@users.noreply.github.com>
2022-03-17 10:10:33 -0500

    misc

--- WebAPI_4/Controllers/WebApiController.cs
+++ WebAPI_4/Controllers/WebApiController.cs
@@ -1,10 +1,5 @@
-﻿using CommonStructures;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using System;
-using System.IO;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace WebAPI_4.Controllers
